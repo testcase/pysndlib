3ba673bf1a3fe64265bef4ee06ef4910636bb8c5
todd ingalls <tingalls@me.com>
2023-06-28 07:58:58 -0700

    changed back to dylib

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ import Cython.Compiler.Options
 Cython.Compiler.Options.annotate = True
 import numpy
 
-import os
-os.environ['LDFLAGS'] = '-framework CoreAudio -framework CoreFoundation -framework CoreMIDI'
+
+
 
 extensions = [
     Extension("pysndlib.sndlib", ["src/pysndlib/sndlib.pyx"], 
-        extra_objects=["/usr/local/lib/libsndlib.a"], 
+        libraries=["sndlib"], 
         extra_compile_args=['-Wno-parentheses-equality'],
         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]),
     Extension("pysndlib.clm", ["src/pysndlib/clm.pyx"], 
-        extra_objects=["/usr/local/lib/libsndlib.a"], include_dirs=[numpy.get_include()], 
+        libraries=["sndlib"], include_dirs=[numpy.get_include()], 
         extra_compile_args=['-Wno-parentheses-equality'],
         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")])
 ]
@@ -27,13 +27,18 @@ if __name__ == '__main__':
 
 
 
+# linking to static lib
+# import os
+# os.environ['LDFLAGS'] = '-framework CoreAudio -framework CoreFoundation -framework CoreMIDI'
+# 
+
 # extensions = [
 #     Extension("pysndlib.sndlib", ["src/pysndlib/sndlib.pyx"], 
-#         libraries=["sndlib"], 
+#         extra_objects=["/usr/local/lib/libsndlib.a"], 
 #         extra_compile_args=['-Wno-parentheses-equality'],
 #         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]),
 #     Extension("pysndlib.clm", ["src/pysndlib/clm.pyx"], 
-#         libraries=["sndlib"], include_dirs=[numpy.get_include()], 
+#         extra_objects=["/usr/local/lib/libsndlib.a"], include_dirs=[numpy.get_include()], 
 #         extra_compile_args=['-Wno-parentheses-equality'],
 #         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")])
 # ]
@@ -44,5 +49,5 @@ if __name__ == '__main__':
 # 
 #         ext_modules = cythonize(extensions, compiler_directives={'language_level': '3str', 'embedsignature' : False})
 #     )
-
-
+# 
+# 
