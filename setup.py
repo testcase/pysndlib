481165c0b1406cfa5e7f063c0824ea17ef358887
todd ingalls <tingalls@me.com>
2023-07-23 00:20:05 -0700

    going to make separate branch for build wheel

--- setup.py
+++ setup.py
@@ -8,61 +8,16 @@ import os
 
 
 
-# extensions = [
-#     Extension("pysndlib.sndlib", ["src/pysndlib/sndlib.pyx"], 
-#         libraries=["sndlib"], include_dirs=[numpy.get_include()], 
-#         extra_compile_args=['-Wno-parentheses-equality'],
-#         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]),
-#     Extension("pysndlib.clm", ["src/pysndlib/clm.pyx"], 
-#         libraries=["sndlib"], include_dirs=[numpy.get_include()], 
-#         extra_compile_args=['-Wno-parentheses-equality'],
-#         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")])
-# ]
-# 
-# if __name__ == '__main__':
-# 
-#     setup(
-# 
-#         ext_modules = cythonize(extensions, compiler_directives={'language_level': '3str', 'embedsignature' : False})
-#     )
-
-
-
-# linking to static lib
-# 
-if sys.platform.startswith("darwin"):
-    os.environ['LDFLAGS'] = '-framework CoreAudio -framework CoreFoundation -framework CoreMIDI'
-
-    extensions = [
-        Extension("pysndlib.sndlib", ["src/pysndlib/sndlib.pyx"], 
-            extra_objects=["./sndlib/libsndlib.a"], 
-            include_dirs=[numpy.get_include(), './sndlib'],
-            extra_compile_args=['-Wno-parentheses-equality'],
-            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]),
-        Extension("pysndlib.clm", ["src/pysndlib/clm.pyx"], 
-            extra_objects=["./sndlib/libsndlib.a"], 
-            include_dirs=[numpy.get_include(), './sndlib'], 
-            extra_compile_args=['-Wno-parentheses-equality'],
-            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")])
-    ]
-
-
-if sys.platform.startswith("linux"):
-    os.environ['LDFLAGS'] = '-lm -ldl -lasound'
-
-    extensions = [
-        Extension("pysndlib.sndlib", ["src/pysndlib/sndlib.pyx"], 
-            extra_objects=["./sndlib/libsndlib.a"], 
-            include_dirs=[numpy.get_include(), './sndlib'],
-            extra_compile_args=['-Wno-parentheses-equality'],
-            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]),
-        Extension("pysndlib.clm", ["src/pysndlib/clm.pyx"], 
-            extra_objects=["./sndlib/libsndlib.a"], 
-            include_dirs=[numpy.get_include(), './sndlib'], 
-            extra_compile_args=['-Wno-parentheses-equality'],
-            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")])
-    ]
-
+extensions = [
+    Extension("pysndlib.sndlib", ["src/pysndlib/sndlib.pyx"], 
+        libraries=["sndlib"], include_dirs=[numpy.get_include()], 
+        extra_compile_args=['-Wno-parentheses-equality'],
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]),
+    Extension("pysndlib.clm", ["src/pysndlib/clm.pyx"], 
+        libraries=["sndlib"], include_dirs=[numpy.get_include()], 
+        extra_compile_args=['-Wno-parentheses-equality'],
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")])
+]
 
 if __name__ == '__main__':
 
@@ -70,5 +25,50 @@ if __name__ == '__main__':
 
         ext_modules = cythonize(extensions, compiler_directives={'language_level': '3str', 'embedsignature' : False})
     )
+
+
+
+# linking to static lib
+# 
+# if sys.platform.startswith("darwin"):
+#     os.environ['LDFLAGS'] = '-framework CoreAudio -framework CoreFoundation -framework CoreMIDI'
+# 
+#     extensions = [
+#         Extension("pysndlib.sndlib", ["src/pysndlib/sndlib.pyx"], 
+#             extra_objects=["./sndlib/libsndlib.a"], 
+#             include_dirs=[numpy.get_include(), './sndlib'],
+#             extra_compile_args=['-Wno-parentheses-equality'],
+#             define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]),
+#         Extension("pysndlib.clm", ["src/pysndlib/clm.pyx"], 
+#             extra_objects=["./sndlib/libsndlib.a"], 
+#             include_dirs=[numpy.get_include(), './sndlib'], 
+#             extra_compile_args=['-Wno-parentheses-equality'],
+#             define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")])
+#     ]
+# 
+# 
+# if sys.platform.startswith("linux"):
+#     os.environ['LDFLAGS'] = '-lm -ldl -lasound'
+# 
+#     extensions = [
+#         Extension("pysndlib.sndlib", ["src/pysndlib/sndlib.pyx"], 
+#             extra_objects=["./sndlib/libsndlib.a"], 
+#             include_dirs=[numpy.get_include(), './sndlib'],
+#             extra_compile_args=['-Wno-parentheses-equality'],
+#             define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]),
+#         Extension("pysndlib.clm", ["src/pysndlib/clm.pyx"], 
+#             extra_objects=["./sndlib/libsndlib.a"], 
+#             include_dirs=[numpy.get_include(), './sndlib'], 
+#             extra_compile_args=['-Wno-parentheses-equality'],
+#             define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")])
+#     ]
+# 
+# 
+# if __name__ == '__main__':
+# 
+#     setup(
+# 
+#         ext_modules = cythonize(extensions, compiler_directives={'language_level': '3str', 'embedsignature' : False})
+#     )
 
 
