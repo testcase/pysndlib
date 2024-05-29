37d955d14c88e6051a0023065a12a1d04c31d7ef
todd ingalls <tingalls@me.com>
2024-05-29 12:34:16 -0700

    starting to add notebooks

--- test/test_clm.py
+++ test/test_clm.py
@@ -1,10 +1,19 @@
 import unittest
 import pysndlib.clm as clm
 
-class SimplisticTest(unittest.TestCase):
+class CLMGeneratorsTest(unittest.TestCase):
+    def test_is_all_pass(self):
+        self.assertTrue(clm.is_all_pass(clm.make_all_pass(.5, .5, 100)))
+        self.assertFalse(clm.is_all_pass(clm.make_oscil(440.)))
+    def test_make_all_pass(self):
+        self.assertIsInstance(clm.make_all_pass(.5, .5, 100), clm.mus_any)
+    def test_all_pass(self):
+        gen = clm.make_all_pass(.5, .5, 100)
+        self.assertEqual(clm.all_pass(gen, 0.0), 0.0)
+        self.assertEqual(clm.all_pass(gen, 1.0), .5)
+        self.assertEqual(clm.all_pass(gen, -1.0), -.5)
 
-    def test(self):
-        self.assertTrue(True)
+        
 
 if __name__ == '__main__':
     unittest.main()
