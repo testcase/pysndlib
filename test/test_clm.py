28c5f108bed80bf394a50392c225d22469a48641
todd ingalls <tingalls@me.com>
2024-05-29 11:37:38 -0700

    starting to add simple unittest test

---
+++ test/test_clm.py
@@ -0,0 +1,12 @@
+import unittest
+import pysndlib.clm as clm
+
+class SimplisticTest(unittest.TestCase):
+
+    def test(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
+    
+    
